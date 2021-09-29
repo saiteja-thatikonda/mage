c61df2bce7bac001fbdb77e5039fba3998b94ded
saiteja-thatikonda <venkata.thatikonda@jivainfotech.com>
2021-09-29 17:09:53 +0530

    sixcommit

---
+++ app/code/Thecoachsmb/Mymodule/Plugin/Checkout/Controller/RestrictProductQuantity.php
@@ -0,0 +1,27 @@
+<?php
+
+ 
+
+    namespace Thecoachsmb\Mymodule\Plugin\Checkout\Controller;
+
+ 
+
+    class RestrictProductQuantity
+
+    {
+
+        public function aroundExecute(
+
+            Index $subject,
+
+            \Closure $proceed
+
+        )
+
+        {
+
+            //You required logic
+
+        }
+
+    }
