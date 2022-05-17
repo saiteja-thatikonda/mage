045bcca2d88d0689c5f695d06bd3f28ffecc0679
saiteja-thatikonda <venkata.thatikonda@jivainfotech.com>
2022-05-17 14:00:01 +0530

    order  task

--- app/code/Efi/BulkOrder/Controller/Index/BulkOrders.php
+++ app/code/Efi/BulkOrder/Controller/Index/BulkOrders.php
@@ -5,27 +5,24 @@ namespace Efi\BulkOrder\Controller\Index;
 use Magento\Framework\Controller\ResultFactory;
 
 class BulkOrders extends \Magento\Framework\App\Action\Action
-{   
-  protected $orderManagementFactory;
-   
-  protected $resultPageFactory;
-    
-   public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-        \Efi\BulkOrder\Model\OrderManagement $orderManagementFactory
-    ) {
-         $this->resultPageFactory = $resultPageFactory;
-         $this->orderManagementFactory = $orderManagementFactory;
+{
+    protected $orderManagementFactory;
+    protected $resultPageFactory;
 
-         return parent::__construct($context);
+    public function __construct(
+        \Magento\Framework\App\Action\Context      $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Efi\BulkOrder\Model\OrderManagement       $orderManagementFactory
+    )
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->orderManagementFactory = $orderManagementFactory
+        return parent::__construct($context);
     }
 
     public function execute()
     {
-       $orderManagement = $this->orderManagementFactory->create();
-   // echo "hi";
-       $orderManagement->createOrder($orderInfo); 
-        
+        $orderManagement = $this->orderManagementFactory->create();
+        $orderManagement->createOrder($orderInfo);
     }
 }
--- app/code/Efi/BulkOrder/Model/OrderManagement.php
+++ app/code/Efi/BulkOrder/Model/OrderManagement.php
@@ -1,15 +1,12 @@
 <?php
 
-
 namespace Efi\BulkOrder\Model;
-
 
 class OrderManagement
 {
     public function _construct(
-    
+
     {
-        \Magento\Framework\App\Action\Context $context,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Customer\Model\CustomerFactory $customerFactory,
         \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
@@ -17,7 +14,6 @@ class OrderManagement
         \Magento\Quote\Model\QuoteFactory $quote,
         \Magento\Quote\Model\QuoteManagement $quoteManagement,
         \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender
-
     ) {
         $this->storeManager = $storeManager;
         $this->customerFactory = $customerFactory;
@@ -26,140 +22,87 @@ class OrderManagement
         $this->quote = $quote;
         $this->quoteManagement = $quoteManagement;
         $this->orderSender = $orderSender;
-        parent::__construct($context);
-        
     }
-        
     }
-    
     public function orderInfo()
-    
-	{
-	$this->orderInfo=$orderInfo;
-	        $orderInfo =[
+    {
+        $orderInfo = [
+            'currency_id' => 'USD',
+            'email' => 'saiteja@gmail.com',
+            'address' => [
+                'firstname' => 'sai',
+                'lastname' => 'Teja',
+                'prefix' => '',
+                'suffix' => '',
+                'street' => 'B1 Abcd street',
+                'city' => 'Los Angeles',
+                'country_id' => 'US',
+                'region' => 'California',
+                'region_id' => '12',
+                'postcode' => '45454',
+                'telephone' => '1234512345',
+                'fax' => '12345',
+                'save_in_address_book' => 1
+            ],
+            'items' =>
+                [
+                    ['product_id' => '1', 'qty' => 1],
+                    ['product_id' => '3', 'qty' => 2, 'super_attribute' => array(93 => 52, 142 => 167)]
+                ]
+        ];
+        return $orderInfo;
+    }
 
-       'currency_id'  => 'USD',
-
-       'email'        => 'saiteja@gmail.com',
-
-        'address' =>[
-
-        'firstname'    => 'sai',
-
-        'lastname'     => 'Teja',
-
-        'prefix' => '',
-
-        'suffix' => '',
-
-        'street' => 'B1 Abcd street',
-
-        'city' => 'Los Angeles',
-
-        'country_id' => 'US',
-
-        'region' => 'California',
-
-        'region_id' => '12', // State region id
-
-        'postcode' => '45454',
-
-        'telephone' => '1234512345',
-
-        'fax' => '12345',
-
-        'save_in_address_book' => 1
-
-        ],
-
-        'items'=>
-
-        [
-
-            ['product_id'=>'1','qty'=>1], //simple product
-
-            ['product_id'=>'3','qty'=>2,'super_attribute' => array(93=>52,142=>167)]
-
-        ]
-
-];
-		return $orderInfo;
-	
-
-            
-	}
-	 
-	public function createOrder($orderInfo) {
-	
+    public function createOrder($orderInfo)
+    {
         $store = $this->storeManager->getStore();
         $storeId = $store->getStoreId();
         $websiteId = $this->storeManager->getStore()->getWebsiteId();
         $customer = $this->customerFactory->create();
         $customer->setWebsiteId($websiteId);
         $customer->loadByEmail($orderInfo['email']);
-        if(!$customer->getId()){
-            
+        if (!$customer->getId()) {
             $customer->setWebsiteId($websiteId)
-                    ->setStore($store)
-                    ->setFirstname($orderInfo['address']['firstname'])
-                    ->setLastname($orderInfo['address']['lastname'])
-                    ->setEmail($orderInfo['email'])
-                    ->setPassword($orderInfo['email']);
+                ->setStore($store)
+                ->setFirstname($orderInfo['address']['firstname'])
+                ->setLastname($orderInfo['address']['lastname'])
+                ->setEmail($orderInfo['email'])
+                ->setPassword($orderInfo['email']);
             $customer->save();
         }
-        $quote=$this->quote->create(); 
-        $quote->setStore($store); 
-        
-        $customer= $this->customerRepository->getById($customer->getId());
+        $quote = $this->quote->create();
+        $quote->setStore($store);
+        $customer = $this->customerRepository->getById($customer->getId());
         $quote->setCurrency();
         $quote->assignCustomer($customer);
-
-       
-        foreach($orderInfo['items'] as $item){
-            $product=$this->productRepository->getById($item['product_id']);
-            if(!empty($item['super_attribute']) ) {
-                
+        foreach ($orderInfo['items'] as $item) {
+            $product = $this->productRepository->getById($item['product_id']);
+            if (!empty($item['super_attribute'])) {
                 $buyRequest = new \Magento\Framework\DataObject($item);
-                $quote->addProduct($product,$buyRequest);
+                $quote->addProduct($product, $buyRequest);
             } else {
-                
-                $quote->addProduct($product,intval($item['qty']));
+                $quote->addProduct($product, intval($item['qty']));
             }
         }
-
-        
         $quote->getBillingAddress()->addData($orderInfo['address']);
         $quote->getShippingAddress()->addData($orderInfo['address']);
-
-       
-        $shippingAddress=$quote->getShippingAddress();
+        $shippingAddress = $quote->getShippingAddress();
         $shippingAddress->setCollectShippingRates(true)
-                        ->collectShippingRates()
-                        ->setShippingMethod('flatrate_flatrate'); 
-        $quote->setPaymentMethod('checkmo'); 
-        $quote->setInventoryProcessed(false); 
-        $quote->save(); 
-        
+            ->collectShippingRates()
+            ->setShippingMethod('flatrate_flatrate');
+        $quote->setPaymentMethod('checkmo');
+        $quote->setInventoryProcessed(false);
+        $quote->save();
         $quote->getPayment()->importData(['method' => 'checkmo']);
- 
-        
         $quote->collectTotals()->save();
-      
         $order = $this->quoteManagement->submit($quote);
-        
         $this->orderSender->send($order);
-        
         $orderId = $order->getIncrementId();
-        if($orderId){
-            $result['success']= $orderId;
-        }else{
-            $result=['error'=>true,'msg'=>'Error occurs for Order placed'];
+        if ($orderId) {
+            $result['success'] = $orderId;
+        } else {
+            $result = ['error' => true, 'msg' => 'Error occurs for Order placed'];
         }
         return $result;
     }
-	
 }
-
-
-
-
